57e44e59822bfcb2e2ea379ce56092f1079725fc
Jhon_M_636 <jonathan636>
2021-04-19 23:31:58 -0500

    v0.3

--- src/app/proveedores/proveedores.component.ts
+++ src/app/proveedores/proveedores.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {Proveedor} from '../proveedor'
+import { FormsModule } from '@angular/forms';
+import { ProveedorService } from '../proveedor.service';
 
 @Component({
-  selector: 'app-proveedores',
+  selector: 'app-Proveedores',
   templateUrl: './proveedores.component.html',
   styleUrls: ['./proveedores.component.css']
 })
 export class ProveedoresComponent implements OnInit {
 
-  constructor() { }
+  proveedor:Proveedor={
+  id:1,
+  name:'Ernesto'
+  };
+
+  selectedProveedor?:Proveedor;
+  proveedores:Proveedor[];
+
+  constructor(private datosProveedor:ProveedorService)
+   {
+
+   }
 
   ngOnInit(): void {
-  }
+
+  this.datosProveedor.getProveedores().subscribe((data:any[])=>{
+
+  console.log(data);
+  this.proveedores=data;
+
+  })
+ }
+
+ onSelect (proveedor:Proveedor): void {
+ this.selectedProveedor=proveedor;
+ }
 
 }
